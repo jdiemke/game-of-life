7c626d7493d7f52446b05ddfa9a7a9902ee3153f
Johannes Diemke <johannes.diemke@eventim.de>
2018-05-22 23:20:30 +0200

    technology stack setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build')
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    }
+}
