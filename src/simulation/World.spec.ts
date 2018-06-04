00210f264276c455424e31a307871d863394a1ab
Johannes Diemke <johannes.diemke@eventim.de>
2018-06-04 02:18:01 +0200

    updates dependencies and adds jasmine spec reporter

--- src/simulation/World.spec.ts
+++ src/simulation/World.spec.ts
@@ -1,7 +1,15 @@
 describe('World', () => {
 
-    it('works', () => {
-        expect(1).toBe(2);
+    it('should work', () => {
+        expect(2).toBe(2);
+    });
+
+    it('should work as well', () => {
+        expect(1).toBe(1);
+    });
+
+    it('should work as well', () => {
+        expect(1).toBe(1);
     });
 
 });
